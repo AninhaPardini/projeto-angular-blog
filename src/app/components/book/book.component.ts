1bbfad910de9f7ea8eb2f81d3588e7ad534f8f0a
AninhaPardini <aninhapardini.work@gmail.com>
2023-10-07 19:41:38 -0300

    ✅ Logica do status bar e ajustes no book

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,23 +1,44 @@
-import { Component, Input } from '@angular/core';
+import { style } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
+import { timeout } from 'rxjs';
+
+interface BookList {
+    cover: string;
+    title: string;
+    note: string;
+    status: string;
+}
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css']
 })
-export class BookComponent {
+export class BookComponent implements OnInit {
+  public books: BookList[] = [
+    {
+      cover: "https://m.media-amazon.com/images/I/61owA5ey3iL._SY445_SX342_.jpg",
+      title: "A revolução dos bixos",
+      note: "4",
+      status: "20%"
+    },
+    {
+      cover: "https://m.media-amazon.com/images/I/71QkHuZFf8L._SY466_.jpg",
+      title: "14 Hábitos de Desenvol-vedores Altamente Produtivos",
+      note: "5",
+      status: "60%"
+    },
 
-  @Input()
-  cover: string = 'https://m.media-amazon.com/images/I/61owA5ey3iL._SY445_SX342_.jpg';
-  @Input()
-  title: string = 'A revolução dos bixos';
-  @Input()
-  note: string = '';
-  @Input()
-  status: number = 10;
+  ];
 
   constructor() {
 
+
   }
+
+  ngOnInit(): void {
+
+  };
+
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
+
   ngOnInit(): void {
+    const controls: NodeListOf<Element> = document.querySelectorAll(".control");
+    let currentItem: number = 0;
+    const items: NodeListOf<Element> = document.querySelectorAll(".item");
+    const maxItems: number = items.length;
+
+    controls.forEach((control: Element) => {
+      control.addEventListener("click", (e: Event) => {
+        let isLeft: boolean = (e.target as Element).classList.contains("arrow-left");
+
+        if (isLeft) {
+          currentItem -= 1;
+        } else {
+          currentItem += 1;
+        }
+
+        if (currentItem >= maxItems) {
+          currentItem = 0;
+        }
+
+        if (currentItem < 0) {
+          currentItem = maxItems - 1;
+        }
+
+        items.forEach((item: Element) => item.classList.remove("current-item"));
+
+        (items[currentItem] as HTMLElement).scrollIntoView({
+          behavior: "smooth",
+          inline: "center"
+        });
+
+        items[currentItem].classList.add("current-item");
+      });
+    });
 
   }
 }
